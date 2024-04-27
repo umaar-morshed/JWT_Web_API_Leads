2af4e114940559df4d67ceff1f420fd076a8493b
Umaar_Abdullah <umaar71@gmail.com>
2024-04-28 00:32:37 +0600

    Add project files.

---
+++ JWT_Web_API_Leads/Controllers/AuthController.cs
@@ -0,0 +1,102 @@
+﻿using JWT_Web_API_Leads.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace JWT_Web_API_Leads.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // stored user
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            // check if user exists
+            if(user.Username != request.Username)
+            {
+                return BadRequest("User not found");
+            }
+
+            if(!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Wrong Password.");
+            }
+
+            string jwtToken = GenerateJwtToken(user);
+            return Ok(jwtToken);
+        }
+
+        private string GenerateJwtToken(User user)
+        {
+            // claims are properties which can help validate the token and can be read with a corresponding client application
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+
+            /* Creating a json web token */
+
+            // creating a key from secret key defined in appsettings
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var sigining_credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(5),
+                signingCredentials: sigining_credentials
+            );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        // out keyword in C# is a parameter modifier that allows you to pass an argument to a method by reference instead of by value
+        // Password salting is a security process that involves adding random strings and integers to passwords before hashing them.
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);    // if ture user has entered the correct username  and password
+            }
+        }
+    }
+}
---
+++ JWT_Web_API_Leads/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace JWT_Web_API_Leads.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }    
+        public byte[] PasswordSalt { get; set; }
+    }
+}
